1267a7bee1ea23e7d5b3f6fa11bc86f0e680be39
mjy <jy@aaa.com>
2024-02-14 17:40:11 +0900

    2024-02-13

--- array/src/array/quiz12.java
+++ array/src/array/quiz12.java
@@ -9,6 +9,6 @@ public class quiz12 {
 		// 입력 : 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100보다 작다.
 		// 출력 : 첫째 줄에 최댓값을 입력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 
-		Scanner sc 
+		Scanner sc = new Scanner(System.in);
 	}
 }
